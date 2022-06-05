2d2e8d36020cd295435891e9b1bdb9e21caa3557
Mariesophie-flament <mariesophie.flament@edu.ece.fr>
2022-06-05 19:49:11 +0200

    Premier essai de page de connexion

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Connexion</title>
+		<meta charset="UTF-8">
+    <link href="assets/css/UX.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="assets/js/vue-connexion.js" async></script>
+    </head>
+    <body>
+        <?php
+
+      ?>
+    </body>
+  </html>
