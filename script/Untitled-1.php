ad478f473d73d79bc6194994307ff15ae7d9080b
victor-favre <91471760+victor-favre@users.noreply.github.com>
2022-06-06 18:41:18 +0200

    Add files via upload

---
+++ script/Untitled-1.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href=".styled {.css" rel="stylesheet" />
+    </head>
+    <body>
+        <a href="Visualisation.php">
+        <input class="favorite styled"
+               type="button"
+               value="Visualisation de l'enregistrement">
+        </a>
+    </body>
+</html>
---
+++ script/Visualisation.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href=".styled {.css" rel="stylesheet" />
+    </head>
+    <body>
+        <strong>
+        <?php echo $titre ?>        <?php echo $_SERVER["HTTP_USER_AGENT"]; ?>
+        <?php echo $datepubli ?>
+        <?php echo $editeur ?>
+        <?php echo $collection ?>
+        <?php echo $edition ?>
+        </strong>
+        <a href="Untitled-1.php">
+        <input class="favorite styled"
+               type="button"
+               value="Retour">
+        </a>
+    </body>
+</html>
