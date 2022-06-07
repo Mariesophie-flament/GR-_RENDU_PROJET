5b38246d2faeb3a638a629c8e0bc531c00bc69f1
capucine5 <91429031+capucine5@users.noreply.github.com>
2022-06-07 17:52:44 +0200

    Update check_creation.php

--- layout/check_creation.php
+++ layout/check_creation.php
@@ -1,5 +1,9 @@
 int titre;
 date datepubli;
+char editeur;
+char collection;
+char edition;
+char nom;
 
 
 
@@ -10,7 +14,8 @@ date datepubli;
     $datepubli = $_POST["Date de publication"];
     $editeur = $_POST["Editeur"]; 
     $collection = $_POST["Collection"];
-    $edition = $_POST["Edition"]; 
+    $edition = $_POST["Edition"];
+    $nom = $_POST["Nom de l'Auteur"];
 
     
     if (!isset($titre)){
@@ -27,22 +32,37 @@ date datepubli;
     }
     if (!isset($edition)){
         die("S'il vous plaît entrez l'édition du livre");
+    }   
+    if (!isset($nom)){
+      die("S'il vous plaît entrez le nom de l'auteur");
     }  
   }
 
   
 
-  $sql=("INSERT INTO `Books`.`Livres` ( `Titre` , `DatePublication` , `Editeur`, `Collection`, `Edition`, `Approuve`)   <!-- on met en bdd ce qui vient d'être remplie
+  $sql=("INSERT INTO `Books`.`Livres` ( `Titre` , `DatePublication` , `Editeur`, `Collection`, `Edition`, `Approuve`)   <!-- on met en bdd ce qui vient d'être rempli
   VALUES ( $titre , $datepubli, $editeur, $collection, $edition, false)");
 
+  $sql=("INSERT INTO `Books`.`LivresAuteurs` (`Livres`, `Auteurs`) 
+  VALUES ( $titre, $nom) ");
   
+  try{                                                                                         //pour le message d'erreur
+    $connexion = new PDO("mysql:host=".$servername. ";dbname=".$dbname, $user, $password);
+    foreach ($connexion->query('SELECT * FROM Utilisateurs') as $row){
+        print(Votre livre a bien été enregistré);
+    }
+    $connexion= null;
+}catch(PDOException $e){
+    print "Erreur!:" . $e->getMessage() ." <br/>" ;
+    die();
+}
   
-    $titre = 'titre';                                                                 
-    //màj des variables pour permettre la Visualisation
-    $datepubli= 'datepubli';
-    $editeur= 'editeur';
-    $collection= 'collection';
-    $edition= 'edition';
-
+    titre= 'titre';                                                                 //màj des variables pour permettre la Visualisation
+    datepubli= 'datepubli';
+    editeur= 'editeur';
+    collection= 'collection';
+    edition= 'edition';
+    auteur= 'nom;'
 
 ?>
+
