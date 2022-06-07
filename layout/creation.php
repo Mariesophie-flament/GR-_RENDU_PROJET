71490f1d73f0572e9cb16c34836936e4374d1926
capucine5 <91429031+capucine5@users.noreply.github.com>
2022-06-07 17:51:30 +0200

    Update creation.php

--- layout/creation.php
+++ layout/creation.php
@@ -3,30 +3,22 @@
     <title>Formulaire pour rentrer un livre</title>
   </head>
   <body>
-    <form method="post" action="creation.php">
+    <form method="post" action="creation.php">                                                  
       Titre : <input type="text" name="Titre" placeholder="Entrez le titre du livre" /><br />
-      DatePubli : <input type="date" name="Date de publication" placeholder="Entrer la date de publication du livre" /><br />
-      Editeur : <input type="name" name="Editeur" placeholder="Entrer l'éditeur du livre" /><br />
-      Collection : <input type="text" name="Collection" placeholder="Entrer la collection du livre" /><br />
-      Edition : <input type="text" name="Edition" placeholder="Entrer l'édition du livre" /><br />
-      <input type="submit" value="Submit" />
+      DatePubli : <input type="date" name="Date de publication" placeholder="Entrez la date de publication du livre" /><br />
+      Editeur : <input type="name" name="Editeur" placeholder="Entrez l'éditeur du livre" /><br />
+      Collection : <input type="text" name="Collection" placeholder="Entrez la collection du livre" /><br />
+      Edition : <input type="text" name="Edition" placeholder="Entrez l'édition du livre" /><br />
+      NomAuteur : <input type="name" name="Nom de l'Auteur" placeholder="Entrez le nom de l'auteur" /><br />
+      <input type="submit" value="SubmitLivres" />
     </form>
   </body>
 
-  <head>
-    <title>Formulaire pour s'inscrire sur le registre</title>
-  </head>
-  <body>
-    <form method="post" action="creation.php">
-      Titre : <input type="text" name="Titre" placeholder="Entrez le titre du livre" /><br />
-      DatePubli : <input type="date" name="Date de publication" placeholder="Entrer la date de publication du livre" /><br />
-      Editeur : <input type="name" name="Editeur" placeholder="Entrer l'éditeur du livre" /><br />
-      Collection : <input type="text" name="Collection" placeholder="Entrer la collection du livre" /><br />
-      Edition : <input type="text" name="Edition" placeholder="Entrer l'édition du livre" /><br />
-      <input type="submit" value="Submit" />
-    </form>
-  </body>
+  
 </html>
+<?php include 'check_creation.php';                                       // exécute le check qui va vérifier si tout est rempli et rentrer en bdd 
+?>                                                                        
+
 
 
 
