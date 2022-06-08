ef79644fde7fd80f32abb04d4e90e7711800fa3f
Mariesophie-flament <91212649+Mariesophie-flament@users.noreply.github.com>
2022-06-08 18:54:54 +0200

    Création Livre mise en forme

--- layout/creation.php
+++ layout/creation.php
@@ -1,16 +1,23 @@
 <html>
   <head>
     <title>Formulaire pour rentrer un livre</title>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+    <link href="../assets/css/form.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="../assets/js/vue-form-create-livre.js" async></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
   </head>
   <body>
     <form method="post" action="creation.php">                                                  
-      Titre : <input type="text" name="Titre" placeholder="Entrez le titre du livre" /><br />
-      DatePubli : <input type="date" name="Date de publication" placeholder="Entrez la date de publication du livre" /><br />
-      Editeur : <input type="name" name="Editeur" placeholder="Entrez l'éditeur du livre" /><br />
-      Collection : <input type="text" name="Collection" placeholder="Entrez la collection du livre" /><br />
-      Edition : <input type="text" name="Edition" placeholder="Entrez l'édition du livre" /><br />
-      NomAuteur : <input type="name" name="Nom de l'Auteur" placeholder="Entrez le nom de l'auteur" /><br />
-      <input type="submit" value="SubmitLivres" />
+      Titre  <input type="text" name="Titre"  /><br />
+      Date de publication <input type="date" name="DatePublication" /><br />
+      Editeur <input type="text" name="Editeur" /><br />
+      Collection  <input type="text" name="Collection" /><br />
+      Edition  <input type="text" name="Edition"  /><br />
+      Nom de l'auteur  <input type="text" name="NomAuteur" /><br /><br>
+      <input type="submit" value="Créer" />
     </form>
   </body>
 
