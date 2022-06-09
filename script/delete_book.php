53041c76721f4fc4907c661533109628b3f34d7a
LynnFloch <91480502+LynnFloch@users.noreply.github.com>
2022-06-09 16:26:28 +0200

    Add files via upload

--- script/delete_book.php
+++ script/delete_book.php
@@ -7,15 +7,7 @@
     </head>
 
     <body>
-        // Reussir à supprimer la bonne table dans la base de données
-            <form method="post" action="dashboard.php">
-        
     <?php
-
-    include("dashboard.php"); 
-
-    $sql="SELECT COUNT(*) AS $recuperation FROM Livres ";
-    echo($recuperation); // VOIR SI CA MARCHE A VIRER
     
     /*if(isset ($_POST["SubmitDelete"])){
         $selected_radio = $_POST['drone']; 
@@ -31,34 +23,36 @@
 
     if (isset($_POST['Delete'])) {
         $sql = "SELECT * FROM Books";
-            if ($titre != "") {
-                $sql .= " WHERE Titre LIKE '%$titre%'"; if ($nomauteur != "") {
-                $sql .= " AND IdAuteurs LIKE '%$nomauteur%'";
-            } }
-            if (mysqli_num_rows($result_search) == 0) {
-            //Livre inexistant
-                echo "Cannot delete. Book not found. <br>"; } 
-            
-            else {
-                while ($data = mysqli_fetch_assoc($result_search) ) { 
-                    $id = $data['ID'];
-                    echo "<br>"; }
-                $sql = "DELETE FROM Books";
-                $sql .= " WHERE ID = $id"; 
-                echo "Delete successful. <br>";
+        if ($titre != "") {
+            $sql .= " WHERE Titre LIKE '%$titre%'"; 
+        }
+        if (mysqli_num_rows($result_search) == 0) {
+        //Livre inexistant
+        echo "Cannot delete. Book not found. <br>"; } 
+        else {
+            while ($data = mysqli_fetch_assoc($result_search) ) { 
+                $id = $data['ID'];
+                echo "<br>"; }
+        $sql = "DELETE FROM Books WHERE ID = valeur"; 
 
-                //on affiche les autres livres dans la BDD
-                $sql = "SELECT * FROM Books";
+        echo "Delete successful. <br>";
 
-                echo "Les livres dans notre bibliothèque: <br>"; 
+        //on affiche les autres livres dans la BDD
+        $sql = "SELECT * FROM Books";
 
-                while ($data = mysqli_fetch_assoc($result_search)) {
-                    echo "ID: " . $data['ID'] . "<br>";
-                } 
-            }
-            echo "Titre: " . $data['Titre'] . "<br>"; echo "Prenom : " . $data['Prenom'] . "<br>"; echo "IdAuteurs: " . $data['IdAuteurs'] . "<br>"; 
-            echo "DatePublication: " . $data['DatePublication'] . "<br>"; echo "Editeur: " . $data['Editeur'] . "<br>"; 
-            echo "Edition: " . $data['Edition'] . "<br>";echo "Collection: " . $data['Collection'] . "<br>"; echo "<br>";
+        while ($data = mysqli_fetch_assoc($result_search)) {
+            echo "ID: " . $data['ID'] . "<br>";
+            echo "Titre: " . $data['Titre'] . "<br>"; 
+            echo "Prenom : " . $data['Prenom'] . "<br>"; 
+            echo "IdAuteurs: " . $data['IdAuteurs'] . "<br>"; 
+            echo "DatePublication: " . $data['DatePublication'] . "<br>"; 
+            echo "Editeur: " . $data['Editeur'] . "<br>"; 
+            echo "Edition: " . $data['Edition'] . "<br>";
+            echo "Collection: " . $data['Collection'] . "<br>"; 
+            echo "<br>";
+        } 
+        }
+    echo "</table>";
     ?>
 
     </body>     
