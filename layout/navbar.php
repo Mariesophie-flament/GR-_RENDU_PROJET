e5fba940539a4b413e2d0a328730fa2be5363832
LynnFloch <91480502+LynnFloch@users.noreply.github.com>
2022-06-09 18:02:35 +0200

    Add files via upload

--- layout/navbar.php
+++ layout/navbar.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
+
 <html>
+  
   <head>
-    <title>Barre de Navigation</title>
+    <title> Barre de Navigation </title>
     <meta charset='utf-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+                                                                <!-- LIEN BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../assets/css/barre.css"> 
   </head>
 
-
   <body>
     <nav class="navbar navbar-expand-md">
+    <!-- Logo de l'entreprise de meme dimension que la barre de navigation --> 
+    <img src="../assets/img/logo.jpeg" width="50" height ="50"/>
 
-      <img src="../assets/img/logo.jpeg" width="50" height ="50"/>
-        <div class="collapse navbar-collapse" id="main-navigation">
-        <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="main-navigation">
 
-          <!--<li class="nav-item"><a class="nav-link " href="#">Connexion</a></li>-->
-          <!--<li class="nav-item"><a class="nav-link " href="#">Inscription</a></li>-->
-          <li class="nav-item"><a class="nav-link " href="creation.php">Enregistrement</a></li> 
-          <!--<li class="nav-item"><a class="nav-link " href="update.php">Mise à jour</a></li>-->
-          <!--<li class="nav-item"><a class="nav-link " href="../script/delete_book.php">Suppression</a></li>-->
+      <ul class="navbar-nav"> <!-- Lien vers la page enregistrement de livre sur la barre de navigation -->
+        <li class="nav-item"><a class="nav-link " href="creation.php">Enregistrement</a></li> 
+      </ul>
 
-        </ul>
-        </div>
-        <a href="../index.php"><input class ="favorite styled" type="button" value="Déconnexion"></a>  
-        <div id="top-navbar"></div>
-
+    </div>
+    <!-- BOUTON DECONNEXION -->
+    <a href="../index.php"><input class ="favorite styled" type="button" value="Déconnexion"></a>  
+    <div id="top-navbar"></div>
     </nav>
   </body>
+
 </html>
+
