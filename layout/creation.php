96cbc1cfeeb5d3023548f053b95e4d28e8e61e20
Mariesophie-flament <91212649+Mariesophie-flament@users.noreply.github.com>
2022-06-09 16:51:03 +0200

    Update creation.php

--- layout/creation.php
+++ layout/creation.php
@@ -10,26 +10,70 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
   </head>
   <body>
-    <form method="post" action="creation.php">                                                  
-      Titre  <input type="text" name="Titre"  /><br />
-      Date de publication <input type="date" name="DatePublication" /><br />
-      Editeur <input type="text" name="Editeur" /><br />
-      Collection  <input type="text" name="Collection" /><br />
-      Edition  <input type="text" name="Edition"  /><br />
-      Nom de l'auteur  <input type="text" name="NomAuteur" /><br /><br>
-      <input type="submit" value="Créer" />
-    </form>
+    <form method="post">                                                  
+      Titre  <input type="text" name="titre"  /><br />
+      Nom de l'auteur  <input type="text" name="nomauteur" /><br />
+      Prénom de l'auteur  <input type="text" name="prenomauteur" /><br />
+      Date de publication <input type="date" name="datepub" /><br />
+      Editeur <input type="text" name="editeur" /><br />
+      Collection  <input type="text" name="collection" /><br />
+      Edition  <input type="text" name="edition"  /><br /><br>
+      <input type="submit" style="color : #fff; background-color: #B12A50" value="Créer" />
+    </form>  
   </body>
+</html>
+
+<?php 
+  include('../script/utils.php');
+  include('../script/bdd_livres_connect.php');
+  include('../script/check_creation.php');
+
+  $titre = isset($_POST["titre"])? $_POST["titre"] : "";
+  $datepub= isset($_POST["datepub"])? $_POST["datepub"] : "";
+  $editeur = isset($_POST["editeur"])? $_POST["editeur"] : "";
+  $collection= isset($_POST["collection"])? $_POST["collection"] : "";
+  $edition = isset($_POST["edition"])? $_POST["edition"] : "";
+  $nomauteur= isset($_POST["nomauteur"])? $_POST["nomauteur"] : "";
+  $prenomauteur= isset($_POST["prenomauteur"])? $_POST["prenomauteur"] : "";
+
+  if ($titre == ""){
+    echo "<p class='erreur'>Le formulaire n'est pas valide.</p>";
+  }
+  else if($datepub == ""){
+    echo "<p class='erreur'>Le formulaire n'est pas valide.</p>";
+  }
+  else if($editeur ==""){
+    echo "<p class='erreur'>Le formulaire n'est pas valide.</p>";
+  }
+  else if($collection ==""){
+    echo "<p class='erreur'>Le formulaire n'est pas valide.</p>";
+  }
+  else if($edition == ""){
+    echo "<p class='erreur'>Le formulaire n'est pas valide.</p>";
+  }
+  else if($nomauteur ==""){
+    echo "<p class='erreur'>Le formulaire n'est pas valide.</p>";
+  }
+  else if($prenomauteur ==""){
+    echo "<p class='erreur'>Le formulaire n'est pas valide.</p>";
+  }
+  else{
+    $sql = "INSERT INTO `Livres` (`Titre`, `DatePublication`, `Editeur`, `Collection`, `Edition`)
+    VALUES('$titre', '$datepub', '$editeur', '$collection', '$edition');";
+    $result_search = executeQuery($connexion, $sql);
+    $sql = "INSERT INTO `Auteurs` (`Nom`,`Prenom`) 
+    VALUES ('$nomauteur','$prenomauteur');";
+    $result_search = executeQuery($connexion, $sql);
+    $sql = "INSERT INTO `LivresAuteurs`(`IdLivres`,`IdAuteurs`) VALUES ('$titre','$nomauteur');";
+    $result_search = executeQuery($connexion, $sql);
+
+    echo "<p class='valide'> L'enregistrement a été enregistré et est en attente d'approbation.</p>";
+    echo "<script type='text/javascript'>document.location.replace('dashboard.php');</script>"; 
+  }
 
   
-</html>
-<?php include 'check_creation.php';                                       // exécute le check qui va vérifier si tout est rempli et rentrer en bdd 
-?>                                                                        
-
-
-
-
-
+                                    
+?> 
 
 
 
