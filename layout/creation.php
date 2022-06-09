eff80556113169e866ac15bbd1d17058303d0258
LynnFloch <91480502+LynnFloch@users.noreply.github.com>
2022-06-09 17:13:27 +0200

    Add files via upload

--- layout/creation.php
+++ layout/creation.php
@@ -1,16 +1,17 @@
 <html>
   <head>
-    <title>Formulaire pour rentrer un livre</title>
-    <meta charset='utf-8'>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
-    <link href="../assets/css/form.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="../assets/js/vue-form-create-livre.js" async></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+      <title> Formulaire pour rentrer un livre </title>
+      <meta charset='utf-8'>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+      <link href="../assets/css/form.css" rel="stylesheet" type="text/css" />
+      <script type="text/javascript" src="../assets/js/vue-form-create-livre.js" async></script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
   </head>
+
   <body>
-    <form method="post">                                                  
+    <form method="post">  <!-- FORMULAIRE POUR LA CREATION D'UN LIVRE -->                                            
       Titre  <input type="text" name="titre"  /><br />
       Nom de l'auteur  <input type="text" name="nomauteur" /><br />
       Prénom de l'auteur  <input type="text" name="prenomauteur" /><br />
@@ -24,10 +25,12 @@
 </html>
 
 <?php 
+    // LIEN VERS LA BDD
   include('../script/utils.php');
   include('../script/bdd_livres_connect.php');
   include('../script/check_creation.php');
 
+  // ENREGISTRE LES DONNÉES RENTRÉES PAR L'UTILISATEUR 
   $titre = isset($_POST["titre"])? $_POST["titre"] : "";
   $datepub= isset($_POST["datepub"])? $_POST["datepub"] : "";
   $editeur = isset($_POST["editeur"])? $_POST["editeur"] : "";
@@ -36,6 +39,7 @@
   $nomauteur= isset($_POST["nomauteur"])? $_POST["nomauteur"] : "";
   $prenomauteur= isset($_POST["prenomauteur"])? $_POST["prenomauteur"] : "";
 
+  // TEST QUI ENVOIE UN MESSAGE D'ERREUR SI UN DES CHAMPS DU FORMULAIRE EST VIDE 
   if ($titre == ""){
     echo "<p class='erreur'>Le formulaire n'est pas valide.</p>";
   }
@@ -57,26 +61,25 @@
   else if($prenomauteur ==""){
     echo "<p class='erreur'>Le formulaire n'est pas valide.</p>";
   }
+
+  // SI TOUS LES CHAMPS SONT REMPLIS ALORS LES DONNÉES DU NOUVEAU LIVRE INSCRIT EN METTANT À JOUR TOUTES LES BDD LIVRESAUTEURS AUTEURS LIVRES
   else{
     $sql = "INSERT INTO `Livres` (`Titre`, `DatePublication`, `Editeur`, `Collection`, `Edition`)
     VALUES('$titre', '$datepub', '$editeur', '$collection', '$edition');";
     $result_search = executeQuery($connexion, $sql);
+
     $sql = "INSERT INTO `Auteurs` (`Nom`,`Prenom`) 
     VALUES ('$nomauteur','$prenomauteur');";
     $result_search = executeQuery($connexion, $sql);
+
     $sql = "INSERT INTO `LivresAuteurs`(`IdLivres`,`IdAuteurs`) VALUES ('$titre','$nomauteur');";
     $result_search = executeQuery($connexion, $sql);
 
+    // LIEN QUI REDIRIGE VERS LE DASHBOARD 
     echo "<p class='valide'> L'enregistrement a été enregistré et est en attente d'approbation.</p>";
     echo "<script type='text/javascript'>document.location.replace('dashboard.php');</script>"; 
   }
 
   
                                     
-?> 
-
-
-
-
-
-
+?>
