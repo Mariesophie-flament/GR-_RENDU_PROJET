be0e3992682f69afd4234802b35cf30b648e7fe4
capucine5 <91429031+capucine5@users.noreply.github.com>
2022-06-09 18:26:20 +0200

    Add files via upload

--- layout/check_creation.php
+++ layout/check_creation.php
@@ -9,53 +9,54 @@ char nom;
 
 <?php
   // Vérifie qu'il provient d'un formulaire
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $titre = $_POST["titre"]; 
-    $datepubli = $_POST["Date de publication"];
-    $editeur = $_POST["Editeur"]; 
-    $collection = $_POST["Collection"];
-    $edition = $_POST["Edition"];
-    $nom = $_POST["Nom de l'Auteur"];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $titre = $_POST["titre"]; 
+        $datepubli = $_POST["Date de publication"];
+        $editeur = $_POST["Editeur"]; 
+        $collection = $_POST["Collection"];
+        $edition = $_POST["Edition"];
+        $nom = $_POST["Nom de l'Auteur"];
 
     
-    if (!isset($titre)){
-      die("S'il vous plaît entrez le titre du livre");
+        if (!isset($titre)){
+            die("S'il vous plaît entrez le titre du livre");
+        }
+        if (!isset($datepubli)){
+            die("S'il vous plaît entrez la date de publication du livre");
+        }
+        if (!isset($editeur)){
+            die("S'il vous plaît entrez l'éditeur du livre");
+        }
+        if (!isset($collection)){
+            die("S'il vous plaît entrez la collection du livre");
+        }
+        if (!isset($edition)){
+            die("S'il vous plaît entrez l'édition du livre");
+        }   
+        if (!isset($nom)){
+          die("S'il vous plaît entrez le nom de l'auteur");
+        }  
     }
-    if (!isset($datepubli)){
-      die("S'il vous plaît entrez la date de publication du livre");
-    }
-    if (!isset($editeur)){
-        die("S'il vous plaît entrez l'éditeur du livre");
-    }
-    if (!isset($collection)){
-        die("S'il vous plaît entrez la collection du livre");
-    }
-    if (!isset($edition)){
-        die("S'il vous plaît entrez l'édition du livre");
-    }   
-    if (!isset($nom)){
-      die("S'il vous plaît entrez le nom de l'auteur");
-    }  
-  }
 
   
 
-  $sql=("INSERT INTO `Books`.`Livres` ( `Titre` , `DatePublication` , `Editeur`, `Collection`, `Edition`, `Approuve`)   <!-- on met en bdd ce qui vient d'être rempli
-  VALUES ( $titre , $datepubli, $editeur, $collection, $edition, false)");
+    $sql=("INSERT INTO `Books`.`Livres` ( `Titre` , `DatePublication` , `Editeur`, `Collection`, `Edition`, `Approuve`)   <!-- on met en bdd ce qui vient d'être rempli
+    VALUES ( $titre , $datepubli, $editeur, $collection, $edition, false)");
 
-  $sql=("INSERT INTO `Books`.`LivresAuteurs` (`Livres`, `Auteurs`) 
-  VALUES ( $titre, $nom) ");
-  
-  try{                                                                                         //pour le message d'erreur
-    $connexion = new PDO("mysql:host=".$servername. ";dbname=".$dbname, $user, $password);
-    foreach ($connexion->query('SELECT * FROM Utilisateurs') as $row){
-        print(Votre livre a bien été enregistré);
+    $sql=("INSERT INTO `Books`.`LivresAuteurs` (`Livres`, `Auteurs`) 
+    VALUES ( $titre, $nom) ");
+    
+    try{                                                                                         //pour le message d'erreur
+      $connexion = new PDO("mysql:host=".$servername. ";dbname=".$dbname, $user, $password);
+      foreach ($connexion->query('SELECT * FROM Utilisateurs') as $row){
+          print(Votre livre a bien été enregistré);
+      }
+      $connexion= null;
     }
-    $connexion= null;
-}catch(PDOException $e){
+    catch(PDOException $e){
     print "Erreur!:" . $e->getMessage() ." <br/>" ;
     die();
-}
+    }
   
     titre= 'titre';                                                                 //màj des variables pour permettre la Visualisation
     datepubli= 'datepubli';
@@ -65,4 +66,3 @@ char nom;
     auteur= 'nom;'
 
 ?>
-
