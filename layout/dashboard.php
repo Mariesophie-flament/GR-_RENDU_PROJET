c8c21ebd88005ab0e03f0946553fde73b36d1a1c
LynnFloch <91480502+LynnFloch@users.noreply.github.com>
2022-06-09 17:33:22 +0200

    Add files via upload

--- layout/dashboard.php
+++ layout/dashboard.php
@@ -1,45 +1,53 @@
 <?php
+    // INCLUSION DE LA BARRE DE NAVIGATION ET DE RECHERCHE
     include("navbar.php"); 
     echo("<br>");
     include("searchForm.php");
     echo("<br>");
     
-    ?>
+?>
 
-<div class ="marge">  
-<a href="update.php" text-align="center"><button style="color : #fff; background-color: #002D72" >Update</button></a>   
- <input type="submit" style="color : #fff; background-color: #002D72" value="Read">
- <input type="submit" style="color : #fff; background-color: #DA291C" value="Delete">
- <?php //include('../script/delete_book.php')?>
+<div class ="marge">
+    <!-- BOUTONS UPDATE READ DELETE -->
+    <a href="update.php" text-align="center"><button style="color : #fff; background-color: #002D72" >Update</button></a>   
+    <input type="submit" style="color : #fff; background-color: #002D72" value="Read">
+    <input type="submit" style="color : #fff; background-color: #DA291C" value="Delete">
+    <?php include('../script/delete_book.php')?>
+
 </div>
 
 <html>
-  <head>
-    <title>Dashboard</title>
-		<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
-    <link href="../assets/css/dashboard.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  </head>
-
-  <body>
-      <form method="post" action="dashboard.php">
-      <?php
-    
-
-	include('../script/utils.php');
-    include('../script/bdd_livres_connect.php'); 
-    $sql = "SELECT * FROM  LivresAuteurs LEFT JOIN Livres ON LivresAuteurs.IdLivres = Livres.titre";
-	$result_search = executeQuery($connexion, $sql);
-		
+    <head>
+        <title>Dashboard</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+        <link href="../assets/css/dashboard.css" rel="stylesheet" type="text/css" />
+                                                        <!-- LIEN BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    </head>
+  
+    <body>
+        <!-- FORMULAIRE -->
+        <form method="post" action="dashboard.php">
+        
+        <?php
+        // LIEN VERS LA BDD
+        include('../script/utils.php');
+        include('../script/bdd_livres_connect.php'); 
+        
+        // AFFICHAGE DU TITRE, EDITEUR ET NOM DE L'AUTEUR LIEN AVEC LA TABLE DE JOINTURE    
+        $sql = "SELECT * FROM  LivresAuteurs LEFT JOIN Livres ON LivresAuteurs.IdLivres = Livres.titre";
+	    $result_search = executeQuery($connexion, $sql);
+        
         if (!$result_search) {
             $connexion = null;
             exit();
         }
 
         if ($result_search->rowCount() > 0) {
+            
             $connexion = false;
             echo "<table>";
             echo "<th >" . "" . "</th>";
@@ -51,10 +59,14 @@
             echo "<td>" . "<br>" . "</td>";
             echo "<td>" . "<br>" . "</td>";
             echo "<th >" . "Editeur " . "</th>";
+            
             foreach ($result_search as $row) {
                 echo "<tr>";
-                ?> <td ><input type="radio" name="drone" value="<?php $result_search?>"> <br/></td>
-                <?php
+        ?>   <!-- BOUTON RADIO DEVANT CHAQUE INFOS DU LIVRE -->   
+        <td ><input type="radio" name="drone" value="<?php $result_search?>"> <br/></td>
+                
+        <?php
+        // AFFICHAGE DU TITRE, EDITEUR ET NOM DE L'AUTEUR
                 echo "<td >" . $row["IdLivres"] . "</td>";
                 echo "<td>" . "<br> " . "</td>";
                 echo "<td>" . "<br>" . "</td>";
@@ -66,9 +78,10 @@
                 echo "</tr>";
             }
             echo "</table>";
-        } else {
+        } 
+        else {
             echo "<p> Aucun résultats trouvés. </p>";
         }
     ?>
     </body>
- </html>
+</html>
