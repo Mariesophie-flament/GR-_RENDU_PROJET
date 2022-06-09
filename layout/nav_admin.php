8530f7db7ef10d0dd65f5847e3a2710d7fe8828e
LynnFloch <91480502+LynnFloch@users.noreply.github.com>
2022-06-09 18:23:32 +0200

    Add files via upload

---
+++ layout/nav_admin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Barre de Navigation</title>
+        <meta charset='utf-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" type="text/css" href="../assets/css/barre.css"> 
+
+                                                <!-- LIEN BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+         
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-md">
+        <!-- Logo de l'entreprise de meme dimension que la barre de navigation --> 
+        <img src="../assets/img/logo.jpeg" width="50" height ="50"/>
+        <div class="collapse navbar-collapse" id="main-navigation">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link " href="creation.php">Enregistrement</a></li> <!-- Lien vers la page enregistrement du livre sur la barre de navigation -->
+                <li class="nav-item"><a class="nav-link " href="users.php">Approbation</a></li> <!--Lien vers la page approbation de l'utilisateur sur la barre de navigation -->
+            </ul>
+        </div>
+        <!-- Bouton deconnexion -->
+        <a href="../index.php"><input class ="favorite styled" type="button" value="Déconnexion"></a> 
+        
+        <div id="top-navbar"></div>
+    </nav>
+  </body>
+</html>
