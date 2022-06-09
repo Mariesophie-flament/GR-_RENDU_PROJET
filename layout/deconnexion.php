a77f0c9777151a857fb98adeec052be723c22817
LynnFloch <91480502+LynnFloch@users.noreply.github.com>
2022-06-09 17:49:40 +0200

    Add files via upload

--- layout/deconnexion.php
+++ layout/deconnexion.php
@@ -1,27 +1,22 @@
-
 <html>
     <head>
         <title>Déconnexion</title>
 		<meta charset="UTF-8">
         <link href="assets/css/UX.css" rel="stylesheet" type="text/css" />
         <script type="text/javascript" src="assets/js/vue-connexion.js" async></script>
-
     </head>
 
     <body>
-
+        <!-- FORMULAIRE -->
         <form action="index.php" method="post">
 
         <?php
          
-         if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-             if(isset($_POST["Submit"] )){
-                session_destroy();
-             }
-         
-
+            if(isset($_POST["Submit"] )){
+                session_destroy();  // FONCTION QUI DÉTRUIT LA SESSION EN COURS LORSQUE LE BOUTON DECONNEXION EST ACTIONNÉ
+            }
         ?>
     </body>
 </html>
-
