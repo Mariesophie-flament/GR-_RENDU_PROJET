2b7ca4f49d975c5a4f9f648c66544da0fd3d1466
LynnFloch <91480502+LynnFloch@users.noreply.github.com>
2022-06-09 18:17:10 +0200

    Update dashboard_admin.php

--- layout/dashboard_admin.php
+++ layout/dashboard_admin.php
@@ -19,7 +19,7 @@ echo("<br>");
 <html>
     <head>
         <title>Dashboard</title>
-		<meta charset="UTF-8">
+	<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
         <link href="../assets/css/dashboard.css" rel="stylesheet" type="text/css" />
 
